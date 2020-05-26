606570ae622bdd6acc27436c9ba38278e5172ea8
Daniel Raymond <dan@raymond.ch>
2020-05-26 11:28:14 -0400

    Push rework 2

---
+++ precache-manifest.c6fcea9ad1358cfbe1ebfea6784ed96c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26d4d9bfb738a6ec940d4b1754d4e7c",
+    "url": "/BlockAI/index.html"
+  },
+  {
+    "revision": "778d7ac7db6a9e6d61d8",
+    "url": "/BlockAI/static/css/main.b45aabf0.chunk.css"
+  },
+  {
+    "revision": "c7721c954e0928a1d53b",
+    "url": "/BlockAI/static/js/2.bcfdd2d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BlockAI/static/js/2.bcfdd2d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778d7ac7db6a9e6d61d8",
+    "url": "/BlockAI/static/js/main.92a6a3ab.chunk.js"
+  },
+  {
+    "revision": "65aa613f16d0d600015a",
+    "url": "/BlockAI/static/js/runtime-main.b840aa00.js"
+  }
+]);
